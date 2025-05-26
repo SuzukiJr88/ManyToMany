6b9264a6e5757604e4463e735bcbb798053e5582
SuzukiJr <36607603+SuzukiJr88@users.noreply.github.com>
2025-05-26 16:20:33 -0300

    The Code

---
+++ src/main/java/com/suzuki/myproject/entities/User.java
@@ -0,0 +1,87 @@
+package com.suzuki.myproject.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_users")
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+
+	//@JsonIgnore
+	@ManyToMany
+	@JoinTable(name="users_cars", joinColumns= {@JoinColumn(name="user_id")}, inverseJoinColumns= {@JoinColumn(name="car_id")})
+	private List<Car> cars = new ArrayList<>();
+
+	public User() {
+
+	}
+
+	public User(Integer id, String name, List<Car> cars) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.cars = cars;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Car> getCars() {
+		return cars;
+	}
+
+	public void setCars(List<Car> cars) {
+		this.cars = cars;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
